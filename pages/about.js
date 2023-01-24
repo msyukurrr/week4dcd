8856246995cfa797e459e449634ee66903add117
msyukurrr <msyukur@my.bcit.ca>
2023-01-24 10:51:35 -0800

    done

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Link from "next/link"
+
+export default function About(){
+    return(
+        <>About Page
+        <Link href="/">Go Back</Link>
+        
+        
+        </>
+    )
+}
